8617cceda973b320bf697d43ca54759e05dd50fe
Natalia <natalia.kielbicka@o2.pl>
2017-01-06 10:53:59 +0100

    singin component, part1

--- src/app/authentication/auth.service.ts
+++ src/app/authentication/auth.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
 import {User} from "./user.model";
+import { Http, Headers, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AuthService {
 
   user: User;
-  constructor() { }
+  constructor(private http: Http) {
+   }
 
   getUser() {
     console.log(this.user);
@@ -14,5 +18,17 @@ export class AuthService {
   addUser(user){
     this.user = user;
   }
+
+   login(username: string, password: string) {
+        return this.http.post('/api/authenticate', JSON.stringify({ username: username, password: password }))
+            .map((response: Response) => {
+                // login successful if there's a jwt token in the response
+                let user = response.json();
+                if (user && user.token) {
+                    // store user details and jwt token in local storage to keep user logged in between page refreshes
+                    localStorage.setItem('currentUser', JSON.stringify(user));
+                }
+            });
+    }
 
 }
--- src/app/authentication/sign-in/sign-in.component.ts
+++ src/app/authentication/sign-in/sign-in.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import {User} from "../user.model";
+import {AuthService} from "../auth.service";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.css']
+  styleUrls: ['./sign-in.component.css'],
+  providers: [AuthService]
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  user:User;
+  returnUrl: string;
+  loading = false;
 
-  ngOnInit() {
+  constructor(private authService: AuthService, private route: ActivatedRoute,
+        private router: Router) {
+      this.authService.getUser();
+   }
+   getUser(){
+    this.authService.getUser()
   }
+    
+  ngOnInit() {
+    this.user = {
+      firstname: '',
+      lastname: '',
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
+      }
+    this.getUser();
+  }
+
+  login() {
+      this.loading = true;
+        this.authService.login(this.user.username, this.user.password)
+            .subscribe(
+                data => {
+                  this.login
+                    this.router.navigate([this.returnUrl]);
+                },
+                error => {
+                    this.loading = false;
+                });
+    }
 
 }
